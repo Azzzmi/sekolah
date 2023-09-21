8d12d34070f8a34f76ecf26fc76f7aad9df2b1eb
Azzzmi <azfadlur@gmail.com>
2023-09-21 10:43:33 +0700

    update pertama

---
+++ front_end/js/akujava.js
@@ -0,0 +1,5 @@
+const manukicon = document.getElementById("menu-ikon");
+const isimenu_anj = document.getElementById("isi-menu")
+manukicon.addEventListener("click",() =>{
+    isimenu_anj.classList.toggle("hilang");
+});
